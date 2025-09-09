2e8af44e0de77ffab6dc88b76c2c93596b4f940f
sftong2000 <sftong2000@hotmail.com>
2025-09-09 11:02:37 -0400

    Update streamlit_app.py
    update line 51 True

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ if ingredients_list :
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe (smoothiefroot_response.json(), use_container_width=true)
+sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
         
         
